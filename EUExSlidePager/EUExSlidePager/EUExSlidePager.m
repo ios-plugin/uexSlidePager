904842a4a519db99b09009ea8f909ac173844409
ygcutter <yangguang@yangguangdeMacBook-Air.local>
2016-01-27 17:48:27 +0800

    openSlidePager方法新增isShowIcon参数

--- EUExSlidePager/EUExSlidePager/EUExSlidePager.m
+++ EUExSlidePager/EUExSlidePager/EUExSlidePager.m
@@ -49,9 +49,7 @@
         jsonStr = [array objectAtIndex:4];
         self.jsonDict = [jsonStr JSONValue];//将JSON类型的字符串转化为可变字典
         isShowIcon = [[self.jsonDict objectForKey:@"isShowIcon"] boolValue];
-    }else{
-        return;
-    }
+       }
     
     if (isShowIcon) {
         _menuV.iconArray = iconArray;
